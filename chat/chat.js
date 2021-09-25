71eab260c470b74533ecac9729ccebcfe6f3c968
Squishy-Penguin <55602651+Squishy-Penguin@users.noreply.github.com>
2021-09-25 17:42:51 -0400

    Added placeholder "I read your message" response

--- chat/chat.js
+++ chat/chat.js
@@ -6,12 +6,13 @@ function downloaded(){
 	send_button = document.getElementById('sendButton');
 
 	function sendMessage(){
+    display_area = document.getElementById('all_sent_messages');
+    
   	send_button.onclick = function () {
-    	display_area = document.getElementById('all_sent_messages');
       var docFragMessage = document.createDocumentFragment();
-      
       var message_to_send = document.createElement('li');
-      message_to_send.style.float = "right";
+      /* message_to_send.style.float = "right"; */
+      /* message_to_send.style.float = "right" */;
       message_to_send.id = "message";
       var message_contents = document.getElementById('newMessage').value;
       message_to_send.innerHTML = message_contents;
@@ -21,9 +22,22 @@ function downloaded(){
         docFragMessage.appendChild(message_to_send);
         display_area.appendChild(docFragMessage);
         
+        document.getElementById('debugMessage').innerHTML = message_to_send; // DEBUG
+        
         //clear typing box
         document.getElementById('newMessage').value = "";
-      }      
+      }
+      
+      /*Bot response*/
+      var docFragMessage_2 = document.createDocumentFragment();
+      var bot_response = document.createElement('p');
+      /* message_to_send.style.float = "right"; */
+      /* bot_response.style.float = "left" */;
+      bot_response.id = "message";
+      var message_contents = "I read your message";
+      bot_response.innerHTML = message_contents;
+      docFragMessage_2.appendChild(bot_response);
+      display_area.appendChild(docFragMessage_2);
     }
   }
   sendMessage();
